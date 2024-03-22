5b511177b9734bd26e0f7002f5a0a303e9b9bdc9
LGtkz <leozingabr@gmail.com>
2024-03-22 08:00:08 -0300

    Atualizando a main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        /*A escolha entre Template e Decorator depende das necessidades específicas
+         do projeto. Template é ideal para definir algoritmos com partes fixas e
+          variáveis, enquanto Decorator é mais adequado para adicionar funcionalidades
+           dinamicamente sem modificar o código original.*/
         Compras compras = new Online(110.00, 5);
         compras.processaCompra();
         Compras loja = new Loja(90, 5);
